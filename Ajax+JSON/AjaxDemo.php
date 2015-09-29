7c491d01fc2e86d50f14ff70689566951a832e22
publicaccount123 <shuhrathalilov1986@gmail.com>
2015-09-29 12:10:41 -0700

    Updata

---
+++ Ajax+JSON/AjaxDemo.php
@@ -0,0 +1,67 @@
+<?php
+
+//header("Content-Type:text/plain;charset=utf-8");
+header("Content-Type:application/json;charset=utf-8");
+//header("Content-Type:text/xml;charset=utf-8");
+//header("Content-Type:text/html;charset=utf-8");
+//header("Content-Type:application/javascript;charset=utf-8");
+
+
+$staff = array
+	(
+		array("name" => "Haley", "number" => "101", "sex" => "Male", "job" =>"Manager"),
+		array("name" => "abcde", "number" => "102", "sex" => "Male", "job" =>"Developer"),
+		array("name" => "vwxyz", "number" => "103", "sex" => "Female", "job" =>"Recruit")
+	);
+
+//$_SERVER is a super global variable
+//$_SERVER["REQUEST_METHOD"] returns request method.
+//if GET method, search particular person in array by keyword.
+//if POST method, create a new person and store it in array.
+
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+	search();
+}else if($_SERVER["REQUEST_METHOD"] == "POST"){
+	create();
+}
+
+function search()
+{
+	//isset check the variable is set or not.
+	//empty check the variable is empty or not.
+	if(!isset($_GET["number"])||empty($_GET["number"]))
+	{
+		echo '{"success":false,"msg":"missing keyword"}';
+		return;
+	}
+
+	global $staff;
+
+	$number = $_GET["number"];
+	$result = '{"success":false, "msg":"Can not find the employee"}';
+
+	//search in array
+	foreach($staff as $value)
+	{
+		if($value["number"] == $number){
+			$result = '{"success":true, "msg":"Find the employee '.$value["name"].'"}';
+			break;
+		}
+	}
+	echo $result;	
+}
+
+function create(){
+	
+	if(!isset($_POST["name"]) || empty($_POST["name"])
+		|| !isset($_POST["number"]) || empty($_POST["number"])
+		|| !isset($_POST["sex"]) || empty($_POST["sex"])
+		|| !isset($_POST["job"]) || empty($_POST["job"]))
+	{
+		echo '{"success":false,"msg":"Missing Value Error"}';
+		return;
+	}
+	//insert the new employee into array
+	echo '{"success":true, "msg":"Create a new employee '.$_POST["name"].'"}';
+}
+?>
